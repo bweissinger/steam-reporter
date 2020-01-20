626965f4f7f4add5f65d86825840bc79be349a1a
Bryan Weissinger <weissingerb@gmail.com>
2020-01-20 03:19:51 -0600

    Add argparse for argument handling from command line

--- steam-reporter.py
+++ steam-reporter.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
+import argparse
+
+def _parse_args():
+    """Use argparse to get args from command line"""
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'config',
+        help='Config file')
+    parser.add_argument(
+        '--quiet',
+        '-q',
+        action='store_true',
+        help='do not print to console'
+    )
+
+    return parser.parse_args()
+
 def main():
+    args = _parse_args()
     return
 
 if __name__ == '__main__':
