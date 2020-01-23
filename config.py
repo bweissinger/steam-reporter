db5baefe3172ab40ff442b8c2753e7dee1c544c6
Bryan Weissinger <weissingerb@gmail.com>
2020-01-23 02:14:16 -0600

    Add fields with assignments using 'object.__setattr__()' method calls. This is required due to the frozen parameter being set to True.

--- config.py
+++ config.py
@@ -1,11 +1,25 @@
 from dataclasses import dataclass
+import configparser
 
 @dataclass(frozen=True)
 class Config:
     """ConfigParser wrapper"""
 
-    # folder: str
+    email_folder: str
+    processes: int
+    database: str
+    server_receive: str
+    server_send: str
 
     def __init__(self, config):
+        
+        try:
+            object.__setattr__(self, 'email_folder', config.get('General', 'Email_Folder'))
+        except configparser.NoOptionError:
+            object.__setattr__(self, 'email_folder', 'All')
+            pass
 
-        # object.__setattr__(self, name, value)
+        object.__setattr__(self, 'processes', config.get('General', 'Processes'))
+        object.__setattr__(self, 'database', config.get('General', 'Database'))
+        object.__setattr__(self, 'server_receive', config.get('Receive', 'Server'))
+        object.__setattr__(self, 'server_send', config.get('Send', 'Server'))
