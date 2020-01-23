b3063739b474d54dc449c01b248f9f7446442a65
Bryan Weissinger <weissingerb@gmail.com>
2020-01-23 03:07:58 -0600

    Add email_account field to Config dataclass.

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     email_folder: str
     processes: int
     database: str
+    email_address: str
     server_receive: str
     server_send: str
     keyring_id: str = 'steam-reporter'
@@ -25,5 +26,6 @@ class Config:
 
         object.__setattr__(self, 'processes', config.get('General', 'Processes'))
         object.__setattr__(self, 'database', config.get('General', 'Database'))
+        object.__setattr__(self, 'email_address', config.get('General', 'Email_Address'))
         object.__setattr__(self, 'server_receive', config.get('Receive', 'Server'))
         object.__setattr__(self, 'server_send', config.get('Send', 'Server'))
