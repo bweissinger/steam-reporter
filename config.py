a39c4e2dbc4d164aca4819a8a3e3e4ea2a6c7ce8
bweissinger <weissingerb@gmail.com>
2020-06-07 23:42:50 -0500

    Remove server send config option.

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ class Config:
     database: str
     email_address: str
     server_receive: str
-    server_send: str
     local_folder: str
     keyring_id: str = 'steam-reporter'
     emails_per_transaction: int = 0
@@ -34,5 +33,4 @@ class Config:
         object.__setattr__(self, 'database', config.get('General', 'Database'))
         object.__setattr__(self, 'email_address', config.get('General', 'Email_Address'))
         object.__setattr__(self, 'server_receive', config.get('Receive', 'Server'))
-        object.__setattr__(self, 'server_send', config.get('Send', 'Server'))
         object.__setattr__(self, 'emails_per_transaction', config.getint('General', 'Emails_Per_Transaction', ))
